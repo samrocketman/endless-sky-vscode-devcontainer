3b4eb2c4cd316c744a380f706ef7a4d8d6de8ec9
Sam Gleske <sam.mxracer@gmail.com>
2022-05-24 11:23:55 -0400

    add environment info and newlines for readability

--- scripts/create-links-from-stacktrace.sh
+++ scripts/create-links-from-stacktrace.sh
@@ -2,6 +2,13 @@
 # Created by Sam Gleske (@samrocketman on GitHub)
 # Created Tue 24 May 2022 10:57:17 AM EDT
 # MIT Licensed
+# DEVELOPMENT ENVIRONMENT
+#     Ubuntu 20.04.4 LTS
+#     Linux 5.13.0-40-generic x86_64
+#     GNU bash, version 5.0.17(1)-release (x86_64-pc-linux-gnu)
+#     GNU Awk 5.0.1, API: 2.0 (GNU MPFR 4.0.2, GNU MP 6.2.0)
+#     grep (GNU grep) 3.4
+#     cat (GNU coreutils) 8.30
 
 # link https://github.com/endless-sky/endless-sky/blob/e7151f423136300f6b528a1717b3e2260aac9b52/.appveyor.yml
 # ENVIRONMENT VARIABLES (can be overridden by user)
@@ -41,4 +48,13 @@ echo
 
 grep -o '[^ ]\+\.cpp:[:0-9]\+' "${tracefile}" |
   awk -F: -v commit="${commit}" -v URL="${ENDLESS_SKY_URL:-}" \
-    '{if(NR == 1){source=" (crash point)";}else{source=""};print "* "URL"/"commit"/source/"$1"#L"$2source}'
+    '
+    {
+      if(NR == 1) {
+        source=" (crash point)";
+      }
+      else {
+        source="";
+      };
+      print "* "URL"/"commit"/source/"$1"#L"$2source
+    }'
